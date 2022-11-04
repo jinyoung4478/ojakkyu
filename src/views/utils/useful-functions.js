651714646e8718b4d2336a24c9e5be33eb969fdb
김진영 <skswls0@daum.net>
2022-11-04 16:04:33 +0900

    Update : account 페이지 구현

--- src/views/utils/useful-functions.js
+++ src/views/utils/useful-functions.js
@@ -8,22 +8,43 @@ export const validateEmail = (email) => {
   return String(email)
     .toLowerCase()
     .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     );
 };
 
 // 숫자에 쉼표를 추가함. (10000 -> 10,000)
 export const addCommas = (n) => {
-  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 // 13,000원, 2개 등의 문자열에서 쉼표, 글자 등 제외 후 숫자만 뺴냄
 // 예시: 13,000원 -> 13000, 20,000개 -> 20000
 export const convertToNumber = (string) => {
-  return parseInt(string.replace(/(,|개|원)/g, ""));
+  return parseInt(string.replace(/(,|개|원)/g, ''));
 };
 
 // ms만큼 기다리게 함.
 export const wait = (ms) => {
   return new Promise((r) => setTimeout(r, ms));
+};
+
+// 클라이언트 사이드 컴포넌트 렌더링
+export const clientSideInclude = () => {
+  window.addEventListener('load', function () {
+    let allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function (el) {
+      let includePath = el.dataset.includePath;
+      if (includePath) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            el.outerHTML = this.responseText;
+          } else if (this.status == 404) {
+          }
+        };
+        xhttp.open('GET', includePath, true);
+        xhttp.send();
+      }
+    });
+  });
 };
