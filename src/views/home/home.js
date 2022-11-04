bc1d496c39d89da69cef664bda37bb9060139fff
Lee Dano <eksh7080@gmail.com>
2022-11-04 20:06:02 +0900

    Detailpage Update

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,16 +1,18 @@
 import { clientSideInclude } from '../utils/useful-functions.js';
-
+import * as Api from "../utils/api.js";
 
 const product = document.querySelector(".product");
+const moveDetail = document.getElementsByClassName("productEvent");
+const DETAIL_PAGE = "/product";
 
-async function getData() {
+async function creatProduct() {
   try {
     const res = await fetch("/api/products");
+    // const res = await Api.get("/api/products");
     const data = await res.json();
-    // const value = data.Product;
-    
+
     data.forEach((tem) => {
-      product.innerHTML += `<li>
+      product.innerHTML += `<li class="productEvent" >
           <img src=${tem.image}>
           <p>${tem.accessory_type}</p>
           <p>${tem.description}</p>
@@ -21,14 +23,34 @@ async function getData() {
           <p>${tem.stone_type}</p>
         </li>`;
     });
+// <a href="${DETAIL_PAGE}"><img class="productEvent" src=${tem.image}></a>
+    saveProduct(data)
   } catch (err) {
     console.log(err);
   }
 }
 
-async function creatProduct() {
-  await getData();
+function saveProduct(productData){
+  // localStorage.setItem("product", JSON.stringify(productData));
+  console.log(productData)
 }
 
+async function startProduct() {
+  await creatProduct();
+}
+
+startProduct();
+
+window.onload = function(){
+
+  Array.from(moveDetail).forEach((tem,idx) => {
+    tem.addEventListener("click", function(e){
+      console.log("asdasdasdasd",e.target,"idididididididx",idx)
+    })
+  })
+  
+};
+
+// 컴포넌트 랜더링
 clientSideInclude();
-creatProduct();
+
