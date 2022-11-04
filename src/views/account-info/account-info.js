b681887ccc66f94d3dd7ce0451fb0e3dd319c840
김진영 <skswls0@daum.net>
2022-11-04 23:57:05 +0900

    Update : info 페이지 사용자 데이터 불러오기 구현

--- src/views/account-info/account-info.js
+++ src/views/account-info/account-info.js
@@ -1,4 +1,48 @@
-import { clientSideInclude } from '../utils/useful-functions.js';
+import * as Api from '../utils/api.js';
+import { clientSideInclude, checkLogin } from '../utils/useful-functions.js';
 
-// 컴포넌트 렌더링
-clientSideInclude();
+// 각 element 바인딩
+const nameInput = document.querySelector('#nameInput');
+const newPasswordInput = document.querySelector('#newPasswordInput');
+const newPasswordConfirmInput = document.querySelector(
+  '#newPasswordConfirmInput',
+);
+const phoneNumberInput = document.querySelector('#phoneNumberInput');
+const saveButton = document.querySelector('#saveButton');
+let userData;
+
+// 로그인 여부 판별
+checkLogin();
+
+// 페이지 렌더링
+renderElements();
+
+async function insertData() {
+  userData = await Api.get('/api/users/info');
+  const { full_name } = userData;
+  nameInput.value = full_name;
+}
+
+function renderElements() {
+  // 컴포넌트 렌더링
+  clientSideInclude();
+  // 해당 유저 정보 불러오기
+  insertData();
+}
+
+function formValidate(data) {
+  console.log(data);
+}
+
+saveButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const name = nameInput.value;
+  const password = newPasswordInput.value;
+  const passwordConfirm = newPasswordConfirmInput.value;
+  const phoneNumber = phoneNumberInput.value;
+
+  const userInputData = { name, password, passwordConfirm, phoneNumber };
+
+  // 입력받은 데이터 유효성 검사
+  formValidate(userInputData);
+});
