902e9f431ce1723b347905f70490fc466f58a132
이유정 <293451@naver.com>
2022-11-05 13:53:08 +0900

    Fix order, sample, router
    기존에 생성되어 있는 router외에 실수로 추가된 router삭제
    기존에 있던 account-order 파일 확인 후 사용

--- src/views/order/order.js
+++
@@ -1,5 +0,0 @@
-import { clientSideInclude } from '../utils/useful-functions.js';
-import { addCommas } from '../utils/useful-functions.js';
-import * as Api from '../utils/api.js';
-
-clientSideInclude();
