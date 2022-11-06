d609979121fb71fb65d6e1382e234e4f3195135f
김진영 <bayajeu@gmail.com>
2022-11-06 22:27:52 +0900

    Update: 비밀번호 입력 모달 창 생성 및 기능 추가

--- src/views/account-info/account-info.js
+++ src/views/account-info/account-info.js
@@ -6,13 +6,18 @@ import {
 } from '../utils/useful-functions.js';
 
 // 각 element 바인딩
+const emailInput = document.querySelector('#emailInput');
 const nameInput = document.querySelector('#nameInput');
-const newPasswordInput = document.querySelector('#newPasswordInput');
-const newPasswordConfirmInput = document.querySelector(
-  '#newPasswordConfirmInput',
-);
+const passwordInput = document.querySelector('#passwordInput');
+const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
 const phoneNumberInput = document.querySelector('#phoneNumberInput');
 const saveButton = document.querySelector('#saveButton');
+// modal
+const modal = document.querySelector('#modal');
+const modalBackground = document.querySelector('#modalBackground');
+const modalCloseButton = document.querySelector('#modalCloseButton');
+const currentPasswordInput = document.querySelector('#currentPasswordInput');
+const saveCompleteButton = document.querySelector('#saveCompleteButton');
 let userData;
 
 // 로그인 여부 판별
@@ -31,45 +36,101 @@ function renderElements() {
 
 async function insertData() {
   userData = await Api.get('/api/users/myInfo');
-  const { full_name, phone_number } = userData;
 
+  //const { fullName, phoneNumber } = userData;
+  const { full_name, phone_number } = userData;
+  userData.password = '';
+
+  emailInput.value = userData.email;
+  //nameInput.value = fullName;
   nameInput.value = full_name;
+  //phoneNumberInput.value = phoneNumber;
   phoneNumberInput.value = phone_number;
 }
 
-function formValidate(data) {
-  console.log(data);
-}
-
 function addAllEvents() {
-  saveButton.addEventListener('click', handleSubmit);
   phoneNumberInput.addEventListener('input', handlePhoneNumberInput);
+  saveButton.addEventListener('click', openModal);
+  modalCloseButton.addEventListener('click', closeModal);
+  document.addEventListener('keydown', keyDownCloseModal);
+  saveCompleteButton.addEventListener('click', updateUserData);
 }
 
-async function handleSubmit(e) {
+async function updateUserData(e) {
   e.preventDefault();
+
   const name = nameInput.value;
-  const password = newPasswordInput.value;
-  const passwordConfirm = newPasswordConfirmInput.value;
+  const password = passwordInput.value;
+  const passwordConfirm = passwordConfirmInput.value;
   const phoneNumber = phoneNumberInput.value;
+  //const currentPassword = currentPasswordInput.value;
+  const current_password = currentPasswordInput.value;
 
-  const data = {
-    full_name: name,
-    password,
-    phone_number: phoneNumber,
-  };
+  if (password && password.length < 4) {
+    return alertError('비밀번호는 최소 4글자 이상입니다.');
+  }
+  if (password && password !== passwordConfirm) {
+    return alertError('비밀번호를 다시 확인해주세요');
+  }
 
-  // 비밀번호 입력 필요
-  data.current_password = '123123123';
+  //const data = { currentPassword };
+  const data = { current_password };
 
-  // 입력받은 데이터 유효성 검사
-  formValidate(data);
+  //if (name !== userData.fullName) {
+  if (name !== userData.full_name && name.length > 1) {
+    //data.fullName = name;
+    data.full_name = name;
+  }
+  // 비밀번호 및 비밀번호 확인 일치 여부
+  if (password !== userData.password) {
+    data.password = password;
+  }
+  //if (phoneNumber && phoneNumber !== userData.phoneNumber) {
+  if (
+    phoneNumber &&
+    phoneNumber !== userData.phone_number &&
+    phoneNumber.length === 13
+  ) {
+    //data.phoneNumber = phoneNumber
+    data.phone_number = phoneNumber;
+  }
 
   const { _id } = userData;
-  console.log(_id);
-  await Api.put('/api/users', _id, data);
+  try {
+    await Api.put('/api/users', _id, data);
+  } catch (err) {
+    alert('회원 정보 수정을 실패하였습니다 : ', err);
+  }
+}
+
+function alertError(message) {
+  closeModal();
+  alert(message);
 }
 
 function handlePhoneNumberInput() {
   phoneNumberInput.value = formatPhoneNumber(this.value);
+}
+
+// Modal 창 열기
+function openModal(e) {
+  e.preventDefault();
+  modal.classList.add('is-active');
+  currentPasswordInput.focus();
+}
+
+// Modal 창 닫기
+function closeModal(e) {
+  if (e) {
+    e.preventDefault();
+  }
+  modal.classList.remove('is-active');
+}
+
+// 키보드로 Modal 창 닫기
+function keyDownCloseModal(e) {
+  // Esc 키
+  if (e.keyCode === 27) {
+    closeModal();
+  }
 }
