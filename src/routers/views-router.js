13acf146b61f9a137d6bea2fbae9693f4da1a2b5
이유정 <293451@naver.com>
2022-11-07 11:33:08 +0900

    Style: change file name

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -17,7 +17,7 @@ viewsRouter.use('/product', serveStatic('product'));
 viewsRouter.use('/product/:product_id', serveStatic('product-detail'));
 viewsRouter.use('/product/category/:category', serveStatic('product'));
 viewsRouter.use('/order', serveStatic('order'));
-viewsRouter.use('/404', serveStatic('error-404'));
+viewsRouter.use('/error', serveStatic('not-found'));
 
 // viewsRouter.use('/product/:productId', serveStatic('product_detail'));
 
