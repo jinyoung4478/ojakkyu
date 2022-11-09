140a14d883e9db485ed4424c1ccb91e861c39985
이유정 <293451@naver.com>
2022-11-10 00:14:20 +0900

    Update account-order.js

--- src/views/account-order/account-order.js
+++ src/views/account-order/account-order.js
@@ -12,7 +12,7 @@ const checkUserOrder = document.querySelector('#checkUserOrder');
 let orderIdToDelete; // 주문 id
 
 // 주문 가져오기
-const res = await fetch('/api/orders/:orderId');
+const res = await fetch('/api/product');
 const orders = await res.json();
 
 for (const order of orders) {
@@ -20,42 +20,26 @@ for (const order of orders) {
   orderIdToDelete = _id;
   //const date = createdAt.split('T')[0];
 
-  console.log('order', order);
   checkUserOrder.innerHTML += `
-      <ul class="orderList" id="order-${_id}">
+      <ul class="orderList" id="${_id}">
       <li>${date}</li>
       <li>${productTitle} / ${price}</li>
       <li>${productName}</li>
-      <li><button class="deleteButton" id="deleteButton-${_id}">주문취소</button></li>
+      <li><button class="deleteButton" id="${_id}">제품삭제</button></li>
       </ul>
       `;
 }
-
+let getproductId = orders.flat(2);
+console.log(getproductId);
 // 주문 삭제
-// checkUserOrder.onclick = function (event) {
-//   if (event.target.className != 'deleteButton') return;
-//   let orderList = event.target.closest('.orderList');
-//   orderList.remove();
+checkUserOrder.onclick = async function (event) {
+  if (event.target.className != 'deleteButton') return;
+  let orderList = event.target.closest('.orderList');
+  orderList.remove();
 
-//   Api.delete('/api/orders/:orderId', orderIdToDelete);
-//   alert('주문 정보가 삭제되었습니다.');
+  const { target: button } = event;
+  console.log(button['id']);
 
-//   // 전역변수 초기화
-//   orderIdToDelete = '';
-// };
-
-try {
-  //const userToDelete = await Api.get('/api/users/myInfo');
-  const { _id } = await Api.get('/api/orders/:orderId');
-
-  // 삭제 진행
-  await Api.delete('/api/orders', _id);
-
-  // 삭제 성공
+  await Api.delete('/:productId', button['id'], { orders });
   alert('주문 정보가 삭제되었습니다.');
-
-  window.location.href = '/account-order';
-} catch (err) {
-  alert(`주문정보 삭제 과정에서 오류가 발생하였습니다: ${err}`);
-  closeModal();
-}
+};
