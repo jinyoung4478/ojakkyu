9a509a76248812a6bcf92a7f32f2f89f6ab7d2e1
이유정 <293451@naver.com>
2022-11-09 12:50:10 +0900

    Update order-result.js

--- src/views/order-result/order-result.js
+++ src/views/order-result/order-result.js
@@ -6,56 +6,44 @@ import {
 
 // 요소(element), input 혹은 상수
 const checkUserOrder = document.querySelector('#checkUserOrder');
-const deleteButton = document.querySelector(`#deleteButton-${_id}`);
+
+// 페이지 로드 시 실행, 삭제할 주문 id를 전역변수로 관리함
+let orderIdToDelete;
 
 checkLogin();
 addAllElements();
-addAllEvents();
 
 // 요소 삽입 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllElements() {
   renderClientSideComponent();
-  insertOrders();
 }
 
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllEvents() {
-  deleteButton.addEventListener('click', deleteThisOrder);
-}
+// 주문 가져오기
+const res = await fetch('/api/product');
+const orders = await res.json();
 
-// 페이지 로드 시 실행, 삭제할 주문 id를 전역변수로 관리함
-let orderIdToDelete;
-async function insertOrders() {
-  const res = await fetch('/api/product');
-  const orders = await res.json();
+for (const order of orders) {
+  const { _id, date, productTitle, price, productName } = order;
+  orderIdToDelete = _id;
+  //const date = createdAt.split('T')[0];
 
-  for (const order of orders) {
-    const { _id, date, productTitle, price, productName } = order;
-    //const date = createdAt.split('T')[0];
-
-    console.log('id', order);
-
-    checkUserOrder.insertAdjacentHTML(
-      'beforeend',
-      `
-        <ul class="orderList" id="order-${_id}">
-          <li>${date}</li>
-          <li>${productTitle} / ${price}</li>
-          <li>${productName}</li>
-          <li><button class="deleteButton" id="deleteButton-${_id}">주문취소</button></li>
-        </ul>
+  console.log('id', order);
+  checkUserOrder.insertAdjacentHTML(
+    'beforeend',
+    `
+      <ul class="orderList" id="order-${_id}">
+      <li>${date}</li>
+      <li>${productTitle} / ${price}</li>
+      <li>${productName}</li>
+      <li><button class="deleteButton" id="deleteButton-${_id}">주문취소</button></li>
+      </ul>
       `,
-    );
-
-    //전역변수에 해당 주문의 id 할당
-    deleteButton.addEventListener('click', () => {
-      orderIdToDelete = _id;
-    });
-  }
+  );
 }
 
-// db에서 주문정보 삭제
-async function deleteThisOrder(e) {
+//전역변수에 해당 주문의 id 할당
+const deleteButton = document.querySelector(`#deleteButton-${orderIdToDelete}`);
+deleteButton.addEventListener('click', async (e) => {
   e.preventDefault();
 
   try {
@@ -75,4 +63,4 @@ async function deleteThisOrder(e) {
   } catch (err) {
     alert(`주문정보 삭제 과정에서 오류가 발생하였습니다: ${err}`);
   }
-}
+});
