2bbf191dd1c27b3499f377b522ee21484c2281b1
김진영 <bayajeu@gmail.com>
2022-11-09 15:40:47 +0900

    Update: 주문 로직 개선

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -1,6 +1,7 @@
 import * as Api from '../utils/api.js';
 import { renderClientSideComponent } from '../utils/useful-functions.js';
 
+const listItems = document.querySelector('#listItems');
 const nameInput = document.querySelector('#nameInput');
 const postalCodeInput = document.querySelector('#postalCodeInput');
 const searchAddressButton = document.querySelector('#searchAddressButton');
@@ -10,6 +11,8 @@ const phoneNumberInput = document.querySelector('#phoneNumberInput');
 const purchaseButton = document.querySelector('#purchaseButton');
 let userData;
 
+// 상품페이지에서 접근했는지, 장바구니에서 접근했는지 구분하기
+
 // 페이지 렌더링
 renderElements();
 addAllEvents();
@@ -17,25 +20,54 @@ addAllEvents();
 function renderElements() {
   // 컴포넌트 렌더링
   renderClientSideComponent();
-
+  // 주문 목록 제품 리스트 렌더링
+  renderProductList();
   // 해당 유저 정보 불러오기
-  insertData();
+  insertUserData();
 }
 
-async function insertData() {
-  userData = await Api.get('/api/users/myInfo');
-  const { fullName, phoneNumber, address } = userData;
-  userData.password = '';
+// 주문 목록 제품 리스트 렌더링
+function renderProductList() {
+  // 주문 목록 데이터 불러오기
+  const orderData = JSON.parse(sessionStorage.getItem('order')).product;
+  // 추가할 주문 목록 데이터를 element로 변환
+  const elementOfProducts = orderData.reduce(
+    (acc, item) =>
+      acc +
+      `
+        <li>
+            <p>${item.name}</p>
+            <p>${item.quantity}</p>
+            <p>${item.price}</p>
+        </li>
+        `,
+    '',
+  );
+  // HTML 이어붙이기
+  listItems.innerHTML += `<ul>${elementOfProducts}</ul>`;
+}
 
-  nameInput.value = fullName;
-  postalCodeInput.value = address.postalCode;
-  addressInput.value = address.address1;
-  addressDetailInput.value = address.address2;
-  phoneNumberInput.value = phoneNumber;
+async function insertUserData() {
+  // 로그인 여부 판별
+  const token = sessionStorage.getItem('token');
+  // 로그인 상태일 경우 유저 데이터 불러오기
+  if (token) {
+    userData = await Api.get('/api/users/myInfo');
+    const { fullName, phoneNumber, address } = userData;
+    // 유저 비밀번호(민감한 정보) 데이터 삭제
+    userData.password = '';
+    // 각 Input 엘레멘트에 데이터 추가
+    nameInput.value = fullName;
+    postalCodeInput.value = address.postalCode;
+    addressInput.value = address.address1;
+    addressDetailInput.value = address.address2;
+    phoneNumberInput.value = phoneNumber;
+  }
 }
 
 function addAllEvents() {
   searchAddressButton.addEventListener('click', searchAddress);
+  purchaseButton.addEventListener('click', handlePurchase);
 }
 
 function searchAddress(e) {
@@ -69,4 +101,10 @@ function searchAddress(e) {
       addressDetailInput.focus();
     },
   }).open();
+}
+
+function handlePurchase(e) {
+  e.preventDefault();
+  // 구매자 데이터 입력이 잘 되었는지 검증
+  // order api 요청
 }
