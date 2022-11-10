891906d19924067ae9fcb2fbc12624f6fd0b13ec
이유정 <293451@naver.com>
2022-11-10 13:18:39 +0900

    Style: 상품 수정 페이지 작업

--- src/views/product-edit/product-edit.js
+++ src/views/product-edit/product-edit.js
@@ -1,8 +1,9 @@
 import * as Api from '/utils/api.js';
+import { renderClientSideComponent } from '/utils/useful-functions.js';
+renderClientSideComponent();
 
 const productUrl = window.location.pathname.split('/');
 const productId = productUrl[productUrl.length - 2];
-
 
 // 요소(element), input 혹은 상수
 const productImageInput = document.querySelector('#productImage');
@@ -16,98 +17,92 @@ const categoryInput = document.querySelector('#category');
 const editButton = document.querySelector('#editButton');
 const deleteButton = document.querySelector('#deleteButton');
 
-
 // 기존 정보를 불러와서 입력란에 선 기입
 async function writeOriginalData() {
-    const product = await Api.get('/api/product', productId);
-    console.log(product)
-    const productName = product.productName;
-    const productTitle = product.productTitle;
-    const description = product.description;
-    const price = product.price;
-    const stoneType = product.stoneType;
-    const category = product.category;
-    const likes = product.likes;
-    const availability = product.availability;
-    const productImage = product.image;
+  const product = await Api.get('/api/product', productId);
+  console.log(product);
+  const productName = product.productName;
+  const productTitle = product.productTitle;
+  const description = product.description;
+  const price = product.price;
+  const stoneType = product.stoneType;
+  const category = product.category;
+  const likes = product.likes;
+  const availability = product.availability;
+  const productImage = product.image;
 
-    productImageInput.value = productImage;
-    productIdInput.value = productId;
-    productNameInput.value = productName;
-    productTitleInput.value = productTitle;
-    descriptionInput.value = description;
-    priceInput.value = price;
-    stoneTypeInput.value = stoneType;
-    categoryInput.value = category;
-    likesInput.value = likes;
-    //availabilityInput.value = availability;
-
+  productImageInput.value = productImage;
+  productIdInput.value = productId;
+  productNameInput.value = productName;
+  productTitleInput.value = productTitle;
+  descriptionInput.value = description;
+  priceInput.value = price;
+  stoneTypeInput.value = stoneType;
+  categoryInput.value = category;
+  likesInput.value = likes;
+  //availabilityInput.value = availability;
 }
 writeOriginalData();
 
-
-
-
 // 제품 수정
 async function handleEdit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const image = productImageInput.value;
-    const editproductId = productIdInput.value;
-    const editproductName = productNameInput.value;
-    const productTitle = productTitleInput.value;
-    const description = descriptionInput.value;
-    const price = priceInput.value;
-    const stoneType = stoneTypeInput.value;
-    const category = categoryInput.value;
-    const likes = likesInput.value;
+  const image = productImageInput.value;
+  const editproductId = productIdInput.value;
+  const editproductName = productNameInput.value;
+  const productTitle = productTitleInput.value;
+  const description = descriptionInput.value;
+  const price = priceInput.value;
+  const stoneType = stoneTypeInput.value;
+  const category = categoryInput.value;
+  const likes = likesInput.value;
 
-    // 상품 수정 API 요청
-    try {
-        const data = {
-            image,
-            editproductId,
-            editproductName,
-            productTitle,
-            description,
-            price,
-            stoneType,
-            category,
-            likes,
-            "availability": true,
-        };
-        console.log(productId);
-        await Api.put('/api/product', `${productId}`, data);
+  // 상품 수정 API 요청
+  try {
+    const data = {
+      image,
+      editproductId,
+      editproductName,
+      productTitle,
+      description,
+      price,
+      stoneType,
+      category,
+      likes,
+      availability: true,
+    };
+    console.log(productId);
+    await Api.put('/api/product', `${productId}`, data);
 
-        alert(`정상적으로 상품이 수정되었습니다.`);
+    alert(`정상적으로 상품이 수정되었습니다.`);
 
-        // 수정된 페이지로이동
-        window.location.href = `/product/${editproductId}`;
-    } catch (err) {
-        console.error(err.stack);
-        alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
-    }
+    // 수정된 페이지로이동
+    window.location.href = `/product/${editproductId}`;
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
 }
 
 //상품 삭제하기
 async function handleDelete(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    //상품을 삭제하시겠습니까???
-    try {
-        const delConfirm = confirm('상품을 삭제하시겠습니까?');
-        if (delConfirm) {
-            await Api.delete('/api/product', `${productId}`, { productId });
-            // 홈페이지로 이동
-            window.location.href = `/`;
-            alert('삭제되었습니다.');
-        }
-    } catch (err) {
-        console.error(err.stack);
-        alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  //상품을 삭제하시겠습니까???
+  try {
+    const delConfirm = confirm('상품을 삭제하시겠습니까?');
+    if (delConfirm) {
+      await Api.delete('/api/product', `${productId}`, { productId });
+      // 홈페이지로 이동
+      window.location.href = `/`;
+      alert('삭제되었습니다.');
     }
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
 }
 
-
-editButton.addEventListener("click", handleEdit)
-deleteButton.addEventListener("click", handleDelete)
+editButton.addEventListener('click', handleEdit);
+deleteButton.addEventListener('click', handleDelete);
