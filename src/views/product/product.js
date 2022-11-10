bf5163c6f9e37638439788a8bb351814eba58753
김진영 <bayajeu@gmail.com>
2022-11-11 02:57:47 +0900

    Update: 페이지네이션 및 탄생석별 필터링 기능

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -6,26 +6,58 @@ const pagelist = document.querySelector('#pagelist');
 const categoryUrl = window.location.pathname.split('/');
 const categoryType =
   categoryUrl[categoryUrl.findIndex((a) => a == 'category') + 1];
-console.log(categoryType);
 const pageUrl = window.location.search;
-console.log('pageUrl', pageUrl);
 const page = Number(pageUrl.split('?page=')[1] || 1);
-console.log('url', pageUrl.split('?page='));
+let totalPage;
+let products;
+const categoryToKorean = {
+  bracelet: '팔찌',
+  ring: '반지',
+  necklace: '목걸이',
+};
+let nowStone = 'all';
+
 // 페이지 렌더링
 renderElements();
 // 이벤트 바인딩
 addAllEvents();
 
 function renderElements() {
+  // 컴포넌트 렌더링
   renderClientSideComponent();
+  // 카테고리 페이지 타이틀 렌더링
+  renderTitle();
+  // 카테고리에 해당하는 상품 리스트 렌더링
   drawCategoryProducts(page);
 }
 
+// 카테고리 페이지 타이틀 렌더링
+function renderTitle() {
+  if (nowStone === 'all') {
+    category.innerText = categoryToKorean[categoryType];
+  } else {
+    category.innerText = `${categoryToKorean[categoryType]} - ${nowStone}`;
+  }
+}
+
+// 카테고리에 해당하는 상품 리스트 렌더링
 async function drawCategoryProducts(page) {
   try {
-    const { totalPage, products } = await Api.get(
-      `/api/product/category/${categoryType}?page=${page}`,
-    );
+    let result;
+    if (nowStone === 'all') {
+      result = await Api.get(
+        `/api/product/category/${categoryType}?page=${page}`,
+      );
+    } else {
+      result = await Api.get(
+        `/api/product/category/${categoryType}?stone=${nowStone}?page=${page}`,
+      );
+    }
+    // 총 페이지 수
+    totalPage = result.totalPage;
+    // 현재 페이지 제품 데이터
+    products = result.products;
+
     productWrapper.innerHTML = products.reduce(
       (acc, item) =>
         acc +
@@ -41,11 +73,12 @@ async function drawCategoryProducts(page) {
       '',
     );
 
+    // 페이지 리스트 렌더링
     for (let i = 1; i <= totalPage; i++) {
       if (i == page) {
-        pagelist.innerHTML += `<a href="/product/category/${categoryType}?page=${i}" style="font-weight:bold">${i}</a>`;
+        pagelist.innerHTML += `<li><a href="/product/category/${categoryType}?page=${i}" style="font-weight:bold">${i}</a></li>`;
       } else {
-        pagelist.innerHTML += `<a href="/product/category/${categoryType}?page=${i}">${i}</a>`;
+        pagelist.innerHTML += `<li><a href="/product/category/${categoryType}?page=${i}">${i}</a></li>`;
       }
     }
   } catch (err) {
@@ -54,111 +87,31 @@ async function drawCategoryProducts(page) {
   }
 }
 
+// 모든 이벤트 핸들러 바인딩
 function addAllEvents() {
-  productWrapper.addEventListener('click', (e) => {
-    const pareLi = e.target.closest('.productEvent');
-    if (pareLi) {
-      location.href = `/product/${pareLi.dataset.id}`;
-    }
-  });
+  birthStoneUl.addEventListener('mousedown', handleBirthStoneFilter);
+  productWrapper.addEventListener('click', handleToProductDetail);
 }
 
-//////////
-// import * as Api from '/utils/api.js';
-// import { renderClientSideComponent } from '/utils/useful-functions.js';
+// 탄생석별로 모아보기 버튼 이벤트 동작
+function handleBirthStoneFilter(e) {
+  const target = e.target.id;
+  // 빈칸 클릭 방지
+  if (target === '') {
+    return;
+  }
+  // 선택된 탄생석으로 타이틀 변경
+  nowStone = target;
+  // 타이틀 리렌더링
+  renderTitle();
+  // 제품 리스트 리렌더링
+  drawCategoryProducts(page);
+}
 
-// const productWrapper = document.querySelector('#productWrapper');
-// const category = document.querySelector('#category');
-// const birthStoneUl = document.querySelector('#birthStoneUl');
-// const categoryUrl = window.location.pathname.split('/');
-// const categoryType = categoryUrl[categoryUrl.length - 2];
-// const categoryToKorean = {
-//   bracelet: '팔찌',
-//   ring: '반지',
-//   necklace: '목걸이',
-// };
-// let productData;
-// let nowStone = 'all';
-
-// // 페이지 렌더링
-// renderElements();
-// // 이벤트 바인딩
-// addAllEvents();
-
-// function renderElements() {
-//   renderClientSideComponent();
-//   renderTitle();
-//   drawCategoryProducts();
-// }
-
-// function renderTitle() {
-//   if (nowStone === 'all') {
-//     category.innerText = categoryToKorean[categoryType];
-//   } else {
-//     category.innerText = `${categoryToKorean[categoryType]} - ${nowStone}`;
-//   }
-// }
-
-// async function drawCategoryProducts() {
-//   try {
-//     productData = await Api.get(`/api/product/category/${categoryType}`);
-//     productWrapper.innerHTML = productData.reduce(
-//       (acc, item) =>
-//         acc +
-//         `
-//         <li class="productEvent" data-id="${item.productId}">
-//             <img src=${item.image}>
-//             <p>${item.description}</p>
-//             <p>${item.price}</p>
-//             <p>${item.productName}</p>
-//             <p>${item.productTitle}</p>
-//         </li>
-//         `,
-//       '',
-//     );
-//   } catch (err) {
-//     alert(`Error: ${err}`);
-//     location.href = '/';
-//   }
-// }
-
-// function addAllEvents() {
-//   birthStoneUl.addEventListener('mousedown', handleBirthStoneFilter);
-//   productWrapper.addEventListener('click', (e) => {
-//     const pareLi = e.target.closest('.productEvent');
-//     if (pareLi) {
-//       location.href = `/product/${pareLi.dataset.id}`;
-//     }
-//   });
-// }
-
-// function handleBirthStoneFilter(e) {
-//   const target = e.target.id;
-//   if (target === '') {
-//     return;
-//   }
-
-//   // 선택된 탄생석으로 타이틀 변경
-//   nowStone = target;
-//   renderTitle();
-
-//   // 상품 필터링
-//   productWrapper.innerHTML = productData.reduce((acc, item) => {
-//     if (item.stoneType === target || target === 'all') {
-//       return (
-//         acc +
-//         `
-//         <li class="productEvent" data-id="${item.productId}">
-//             <img src=${item.image}>
-//             <p>${item.description}</p>
-//             <p>${item.price}</p>
-//             <p>${item.productName}</p>
-//             <p>${item.productTitle}</p>
-//         </li>
-//         `
-//       );
-//     } else {
-//       return acc;
-//     }
-//   }, '');
-// }
+// 제품 클릭 시 해당 상세 페이지로 이동
+function handleToProductDetail(e) {
+  const pareLi = e.target.closest('.productEvent');
+  if (pareLi) {
+    location.href = `/product/${pareLi.dataset.id}`;
+  }
+}
