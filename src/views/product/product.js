342b362a27ace7b1eaff4d39931337c4c8f62b6b
김진영 <bayajeu@gmail.com>
2022-11-10 15:10:22 +0900

    카테고리 상품 목록 클릭 기능 구현

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -1,52 +1,48 @@
-import * as Api from "/utils/api.js";
-import { renderClientSideComponent } from "/utils/useful-functions.js";
+import * as Api from '/utils/api.js';
+import { renderClientSideComponent } from '/utils/useful-functions.js';
 
-
-const product = document.querySelector(".product");
+const product = document.querySelector('.product');
 const categoryUrl = window.location.pathname.split('/');
 const categoryType = categoryUrl[categoryUrl.length - 2];
-const categoryTitle = document.querySelector(".categoryTitle");
-const categoryWrap = document.querySelector(".categoryWrap");
+
+start();
+
+function start() {
+  renderClientSideComponent();
+  drawCategoryList();
+}
 
 async function drawCategoryList() {
   try {
-
     const res = await fetch(`/api/product/category/${categoryType}`);
     const data = await res.json();
 
-    console.log(data)
+    product.innerHTML = data
+      .map((tem) => {
+        const img = tem.image;
+        const price = tem.price;
+        const title = tem.productTitle;
+        const name = tem.productName;
+        const description = tem.description;
+        const productId = tem.productId;
 
-    product.innerHTML = data.map((tem) => {
-      const img = tem.image;
-      const price = tem.price;
-      const title = tem.productTitle;
-      const name = tem.productName;
-      const description = tem.description;
-
-      return `
-                    <li class="productEvent">
-                        <img src=${img}>
-                        <p>${description}</p>
-                        <p>${price}</p>
-                        <p>${name}</p>
-                        <p>${title}</p>
-                    </li>
-            `
-
-    }).join("")
-
+        return `
+              <li class="productEvent" data-id="${productId}">
+                  <img src=${img}>
+                  <p>${description}</p>
+                  <p>${price}</p>
+                  <p>${name}</p>
+                  <p>${title}</p>
+              </li>
+            `;
+      })
+      .join('');
   } catch (err) {
-    console.log(err);
+    alert(`Error: ${err}`);
   }
 }
 
-async function start() {
-  await renderClientSideComponent();
-  await
-
-    drawCategoryList();
-}
-
-
-
-start();
+product.addEventListener('click', (e) => {
+  const pareLi = e.target.closest('.productEvent');
+  location.href = `/product/${pareLi.dataset.id}`;
+});
