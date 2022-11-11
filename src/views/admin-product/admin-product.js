dc8c2826a8a3f459a5f1fe650722863c9daa2f42
eksh7080 <eksh7080@naver.com>
2022-11-12 02:35:18 +0900

    Update

--- src/views/admin-product/admin-product.js
+++ src/views/admin-product/admin-product.js
@@ -1,40 +1,32 @@
-import { renderClientSideComponent } from "/utils/useful-functions.js";
-import * as Api from "/utils/api.js";
+import { renderClientSideComponent } from '/utils/useful-functions.js';
+import * as Api from '/utils/api.js';
 renderClientSideComponent();
 
+const product = document.querySelector('.product');
 
-const product = document.querySelector(".product");
-const token = sessionStorage.getItem('token');
-let isAdmin = false;
+async function drawProduct() {
+  const res = await Api.get('/api/product/allProducts');
+  product.innerHTML = res
+    .map((tem) => {
+      const { price, productTitle, createdAt, category, productId } = tem;
 
-try{
-    const { role } = await Api.get('/api/users/myInfo');
-    if (role === 'admin-user') isAdmin = true;
-  }catch(err){
-    alert(`Error: ${err}`);
-  }
-
-async function drawProduct(){
-    const res = await Api.get("/api/product/allProducts");
-    product.innerHTML = res.map(tem => {
-        const { price, productTitle, createdAt, category, productId } = tem;
-
-        return `
+      return `
             <ul style="display: flex; gap: 40px;" class="allProduct">
                 <li>${createdAt}</li>
                 <li>${category}</li>
                 <li>${price}</li>
                 <li>${productTitle}</li>
                 <li style="cursor:pointer;">
-                    ${ isAdmin ? `<a href="/product/add" style="color: coral;">상품 추가</a>` : ``}
+                    <a href="/product/add" style="color: coral;">상품 추가</a>
                 </li>
                 <li cursor:pointer;">
-                    ${ isAdmin ? `<a href="/product/edit/${productId}" style="color: coral;">상품수정/삭제</a>` : ``}
+                    <a href="/product/edit/${productId}" style="color: coral;">상품수정/삭제</a>
                 </li>
             </ul>
         
-        `
-    }).join(" ");
+        `;
+    })
+    .join(' ');
 }
 
 drawProduct();
--- src/views/components/header.js
+++ src/views/components/header.js
@@ -6,6 +6,7 @@ const token = sessionStorage.getItem('token');
 
 // gnb
 const gnbContents = document.querySelector('.gnbContents');
+let isAdmin = false;
 
 await drawCategory();
 
@@ -19,10 +20,18 @@ async function drawCategory() {
   }
 
   if (token) {
-    
+    try {
+      const { role } = await Api.get('/api/users/myInfo');
+      if (role === 'admin-user') isAdmin = true;
+    } catch (err) {
+      alert(`Error: ${err}`);
+    }
+
     utilContents.innerHTML = `
  
-            <li><a href="/admin">페이지관리</a></li>
+            <li>
+              ${isAdmin ? `<a href="/admin">페이지관리</a>` : ``}
+            </li>
             <li class="logout">로그아웃</li>
             <li><a href="/account">마이페이지</a></li>
             <li><a href="/cart">장바구니</a></li>
@@ -53,4 +62,3 @@ function moveCategory(e) {
   }
 }
 gnbContents.addEventListener('click', moveCategory);
-
